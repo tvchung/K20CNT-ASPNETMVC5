9336dba2d27f84b07ffdee5913e4209447538717
Chung Trinh <hna.tvchung@gmail.com>
2022-03-22 14:34:05 +0700

    2202-03-22

---
+++ Lesson02/Lesson02/Controllers/ProductController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lesson02.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        public ActionResult Index()
+        {
+            // code logic cho action index [ /productt/index ]
+            string[] Names = { "Hùng", "Dũng", "Sang", "Trọng" };
+            ViewBag.Data = Names;
+
+            return View();
+        }
+
+        // Tạo action ProductDetail
+        public ActionResult ProductDetail(string id)
+        {
+            // Code logi
+
+            ViewBag.Data = id;
+
+            return View();
+        }
+
+        // Kiểu trả về string
+        public string GetName()
+        {
+            return "Đây là sản phẩm Iphone XS 15";
+        }
+
+        public JsonResult GetNames()
+        {
+            string[] Names = { "Hùng", "Dũng", "Sang", "Trọng" };
+            return Json(Names, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
